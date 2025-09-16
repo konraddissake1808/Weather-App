4516a0309daeb1ee4b36e86fb4c636d01d3f855c
konraddissake1808 <konrad.dissakengando@gmail.com>
2025-09-16 18:52:11 +0100

    Pulled all the weather variables to be displayed in the project

--- weather-app-main/src/app/page.tsx
+++ weather-app-main/src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { weatherData } from "../lib/meteo";
 
 export default function Home() {
+  
+  console.log(weatherData);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       
