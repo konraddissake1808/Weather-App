43b9eb9317b60f40fee5f40fd0a2bb4b1a5a7d4b
konraddissake1808 <konrad.dissakengando@gmail.com>
2025-09-17 01:10:16 +0100

    Imported the necessary assets

--- weather-app-main/src/lib/meteo.ts
+++ weather-app-main/src/lib/meteo.ts
@@ -3,7 +3,8 @@ import { fetchWeatherApi } from 'openmeteo';
 const params = {
 	"latitude": 52.52,
 	"longitude": 13.41,
-	"hourly": ["temperature_2m", "relativehumidity_2m", "apparent_temperature", "precipitation", "weathercode", "windspeed_10m"],
+	"hourly": ["temperature_2m", "relativehumidity_2m", "apparent_temperature", "precipitation", "weathercode", "windspeed_10m", "uv_index"],
+    "daily": ["sunrise", "sunset"],
 };
 const url = "https://api.open-meteo.com/v1/forecast";
 const responses = await fetchWeatherApi(url, params);
@@ -24,6 +25,11 @@ console.log(
 );
 
 const hourly = response.hourly()!;
+const daily = response.daily()!;
+
+// Extract sunrise and sunset variables from daily
+const sunrise = daily.variables(0)!;
+const sunset = daily.variables(1)!;
 
 // Note: The order of weather variables in the URL query and the indices below need to match!
 export const weatherData = {
@@ -37,7 +43,21 @@ export const weatherData = {
         precipitation: hourly.variables(3)!.valuesArray(),
         weathercode: hourly.variables(4)!.valuesArray(),
         windspeed_10m: hourly.variables(5)!.valuesArray(),
+        uv_index: hourly.variables(6)!.valuesArray(),
 	},
+    daily: {
+        time: [...Array((Number(daily.timeEnd()) - Number(daily.time())) / daily.interval())].map(
+            (_, i) => new Date((Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) * 1000)
+        ),
+        // Map Int64 values to according structure
+		sunrise: [...Array(sunrise.valuesInt64Length())].map(
+			(_, i) => new Date((Number(sunrise.valuesInt64(i)) + utcOffsetSeconds) * 1000)
+		),
+		// Map Int64 values to according structure
+		sunset: [...Array(sunset.valuesInt64Length())].map(
+			(_, i) => new Date((Number(sunset.valuesInt64(i)) + utcOffsetSeconds) * 1000)
+		),
+    }
 };
 
 // 'weatherData' now contains a simple structure with arrays with datetime and weather data
