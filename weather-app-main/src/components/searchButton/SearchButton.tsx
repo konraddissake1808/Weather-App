beb2e0a0ad9c3623dda9a2e73e602d95a83fe426
konraddissake1808 <konrad.dissakengando@gmail.com>
2025-12-30 15:02:35 +0100

    Added transition-duration to search button

--- weather-app-main/src/components/searchButton/SearchButton.tsx
+++ weather-app-main/src/components/searchButton/SearchButton.tsx
@@ -8,7 +8,7 @@ function SearchButton({searchButtonClick} : SearchButtonProps ) {
   return (
     <div className='w-full mt-3'>
         <div className='w-full'>
-            <button onClick={() => searchButtonClick('', 0)} className='w-full bg-blue-500 h-14 rounded-xl hover:bg-blue-700 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-white'>Search</button>
+            <button onClick={() => searchButtonClick('', 0)} className='w-full bg-blue-500 h-14 rounded-xl hover:bg-blue-700 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-white duration-150'>Search</button>
         </div>
     </div>
   )
